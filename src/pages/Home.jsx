ff655e0b9553683ce65958cf05dec535313bada2
muhammadrayhanr <mrayhanr4869@gmail.com>
2023-06-19 16:32:31 +0700

    integrasi api openai dan .env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,7 @@ import { useSnapshot } from 'valtio';
 
 import state from '../store';
 import { CustomButton } from '../components';
-import {
-  headContainerAnimation,
-  headContentAnimation,
-  headTextAnimation,
-  slideAnimation
-} from '../config/motion';
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -17,40 +12,28 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
-          <motion.header {...slideAnimation("down")}>
-            <img 
-              src='./threejs.png'
-              alt="logo"
-              className="w-8 h-8 object-contain"
-            />
+          <motion.header {...slideAnimation('down')}>
+            <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain" />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                YUK, <br className="xl:block hidden" /> COBAIN.
               </h1>
             </motion.div>
-            <motion.div
-              {...headContentAnimation}
-              className="flex flex-col gap-5"
-            >
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+                Buat desain baju unik kamu sendiri. <strong>Gunakan imajinasimu dan wujudkan gayamu.</strong> Jangan ragu untuk bertanya pada AI yang cerdas jika kamu kesulitan menentukan desainmu.
               </p>
 
-              <CustomButton 
-                type="filled"
-                title="Customize It"
-                handleClick={() => state.intro = false}
-                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-              />
+              <CustomButton type="filled" title="Kustomisasi" handleClick={() => (state.intro = false)} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
             </motion.div>
           </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
